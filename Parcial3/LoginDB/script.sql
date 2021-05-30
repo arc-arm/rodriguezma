79943bcf6f115a92b8bf744a807f365d2f7d0a6a
arc-arm <L18100227@nlaredo.tecnm.mx>
2021-05-30 12:54:21 -0500

    correccion sql

--- Parcial3/LoginDB/script.sql
+++ Parcial3/LoginDB/script.sql
@@ -6,10 +6,10 @@ debe coincidir con el encriptado, en mi caso, 129*/
 
 CREATE TABLE IF NOT EXISTS Usuario 
 (
-    id_usuario TINYINT NOT NULL AUTO_INCREMENT,
+    id_usuario TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user VARCHAR(20) NOT NULL,
     password VARCHAR(129) NOT NULL  
 );
 
 -- Usuario de prueba
-INSERT INTO Usuario (user, password) VALUES ('jose', 'rodriguez');
+INSERT INTO Usuario (user, password) VALUES ('jose', '55bcedb34c8dc6609f48760ff91cab46c15d6a0c55bc5595e1f74db59bc542ea6f7f2c5befa3136ad8115fa1984e00857abc685d8120d4944fc015827fc70a5b');
